0174cad3edc57beea1aba819c0b89fdd8408efe3
Shaikh Safeer <64704877+safeer-shaikh@users.noreply.github.com>
2021-01-30 13:45:52 +0500

    Services Section Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import Feature from './Components/Features/Features'
+import Services from './Components/Services/Services'
 import './Assets/fontawesome-pro-5.15.1-web/css/all.css'
 export default function App() {
   return (
     <div>
       <Header />
       <Feature />
+      <Services />
     </div>
   )
 }
---
+++ src/Components/Services/Services.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Services.css'
+import Service1 from '../../Assets/Images/pic-1.jpg';
+import Service2 from '../../Assets/Images/pic-2.jpg';
+import Service3 from '../../Assets/Images/pic-3.jpg';
+import Service4 from '../../Assets/Images/pic-4.jpg';
+export default function Services() {
+    return (
+        <>
+            <section id='service'>
+                <div className='title-text'>
+                    <p>SERVICES</p>
+                    <h1>We Provide Better</h1>
+                </div>
+                <div className='service-box'>
+                    <div className='single-service'>
+                        <img src={Service1} alt='Service-1' />
+                        <div className='overlay'></div>
+                        <div className='service-desc'>
+                            <h3>Hair Styling</h3>
+                            <hr />
+                            <p>This is test under description of barber 
+                            foundation this is test under description</p>
+                        </div>
+                    </div>
+                    <div className='single-service'>
+                        <img src={Service2} alt='Service-2' />
+                        <div className='overlay'></div>
+                        <div className='service-desc'>
+                            <h3>Beared Trim</h3>
+                            <hr />
+                            <p>This is test under description of barber 
+                            foundation this is test under description</p>
+                        </div>
+                    </div>
+                    <div className='single-service'>
+                        <img src={Service3} alt='Service-3' />
+                        <div className='overlay'></div>
+                        <div className='service-desc'>
+                            <h3>Hair Cut</h3>
+                            <hr />
+                            <p>This is test under description of barber 
+                            foundation this is test under description</p>
+                        </div>
+                    </div>
+                    <div className='single-service'>
+                        <img src={Service4} alt='Service-4' />
+                        <div className='overlay'></div>
+                        <div className='service-desc'>
+                            <h3>Dry Shampoo</h3>
+                            <hr />
+                            <p>This is test under description of barber 
+                            foundation this is test under description</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
